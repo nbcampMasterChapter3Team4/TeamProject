46926bea42c406216684a67c35d5e7ccc6768a4c
cjs1399 <cjs1399@naver.com>
2025-04-07 19:23:07 +0900

    [Setting] #4 - add ImageLiterals

---
+++ TeamProject/TeamProject/Global/Literals/ImageLiterals.swift
@@ -0,0 +1,12 @@
+//
+//  ImageLiterals.swift
+//  TeamProject
+//
+//  Created by 천성우 on 4/7/25.
+//
+
+import UIKit
+
+enum ImageLiterals {
+    static var test: UIImage { .load(name: "test").withRenderingMode(.alwaysOriginal)}
+}
