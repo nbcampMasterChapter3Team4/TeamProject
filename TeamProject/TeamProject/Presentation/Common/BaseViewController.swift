e24a3e8f24e414a55077aa62cbee8d37984b7782
cjs1399 <cjs1399@naver.com>
2025-04-07 19:23:51 +0900

    [Setting] #4 - add Base Common

---
+++ TeamProject/TeamProject/Presentation/Common/BaseViewController.swift
@@ -0,0 +1,36 @@
+//
+//  BaseViewController.swift
+//  TeamProject
+//
+//  Created by 천성우 on 4/7/25.
+//
+
+import UIKit
+
+
+import SnapKit
+import Then
+
+class BaseViewController: UIViewController {
+    
+    private lazy var viewControllerName = self.className
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setLayout()
+        setDelegates()
+        setRegister()
+    }
+    
+    /// View 의 Layout 을 set 합니다.
+    func setLayout() {}
+    /// View 의 Delegate 을 set 합니다.
+    func setDelegates() {}
+    /// View 의 Register 를 set 합니다.
+    func setRegister() {}
+    
+    deinit {
+        print("🧶 \(viewControllerName) is deinited")
+    }
+}
+
