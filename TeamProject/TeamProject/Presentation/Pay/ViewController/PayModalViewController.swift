2c9ef3b469d7842cb01aa0dd769d0ee3f087de2e
cjs1399 <cjs1399@naver.com>
2025-04-08 16:41:26 +0900

    [Feat] #11 - 구현한 ShoppingItemView를 PayModalVC에 연결합니다.

--- TeamProject/TeamProject/Presentation/Pay/ViewController/PayModalViewController.swift
+++ TeamProject/TeamProject/Presentation/Pay/ViewController/PayModalViewController.swift
@@ -18,11 +18,14 @@ class PayModalViewController: BaseViewController {
         $0.setImage(ImageLiterals.iCon.close_button_lightMode_ic, for: .normal)
     }
     
+    private let testView = ShoppingItemView()
+    
     private let bottomButtonView = CustomBottomButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setBottomButton()
+        setAddTarget()
     }
     
     override func setStyles() {
@@ -30,7 +33,7 @@ class PayModalViewController: BaseViewController {
     }
     
     override func setLayout() {
-        view.addSubviews(deleteButton, popButton, bottomButtonView)
+        view.addSubviews(deleteButton, popButton, testView, bottomButtonView)
         
         deleteButton.snp.makeConstraints {
             $0.top.equalToSuperview().offset(50)
@@ -45,14 +48,33 @@ class PayModalViewController: BaseViewController {
             $0.height.width.equalTo(SizeLiterals.Screen.screenHeight * 30 / 874)
         }
         
+        testView.snp.makeConstraints {
+            $0.top.equalTo(deleteButton.snp.bottom).offset(26)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(SizeLiterals.Screen.screenWidth * 366 / 402)
+            $0.height.equalTo(SizeLiterals.Screen.screenHeight * 99 / 874)
+        }
+        
         bottomButtonView.snp.makeConstraints {
             $0.horizontalEdges.equalToSuperview()
             $0.bottom.equalToSuperview()
         }
     }
     
+    func setAddTarget() {
+        testView.getItemCountStepper().addTarget(self, action: #selector(stepperValueChanged), for: .valueChanged)
+    }
+    
     func setBottomButton() {
         bottomButtonView.configure("₩190,000", "결제하기")
     }
     
+    
+    // MARK: - @objc Methods
+    
+    @objc
+    private func stepperValueChanged(_ sender: UIStepper) {
+        let currentValue = Int(sender.value)
+        testView.getItemCountLabel().text = "\(currentValue)"
+    }
 }
