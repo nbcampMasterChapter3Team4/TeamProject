3eca4b8f21730bacf979e6c3daae38342f61b360
seoyounghan <happyunbd364@naver.com>
2025-04-08 15:37:19 +0900

    [Feat] #9 - 모달 띄우기용 임시 버튼 메인뷰에 추가

---
+++ TeamProject/TeamProject/Presentation/DetailModal/ViewController/DetailModalViewController.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+import SnapKit
+import Then
+
+class DetailModalViewController: UIViewController {
+
+    private lazy var viewControllerName = self.className
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setStyles()
+        setLayout()
+        setDelegates()
+        setRegister()
+    }
+
+    /// View 의 Style 을 set 합니다.
+    func setStyles() {}
+    /// View 의 Layout 을 set 합니다.
+    func setLayout() {}
+    /// View 의 Delegate 을 set 합니다.
+    func setDelegates() {}
+    /// View 의 Register 를 set 합니다.
+    func setRegister() {}
+
+    deinit {
+        print("🧶 \(viewControllerName) is deinited")
+    }
+}
+
---
+++ TeamProject/TeamProject/Presentation/DetailModal/Views/DetailModalView.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+import SnapKit
+import Then
+
+class DetailModalView: UIView {
+
+    private lazy var viewName = self.className
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setLayout()
+    }
+
+    override func removeFromSuperview() {
+        super.removeFromSuperview()
+        print("🧵 \(viewName) has been successfully Removed")
+    }
+
+    /// View 의 Layout 을 set 합니다.
+    func setLayout() {}
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
