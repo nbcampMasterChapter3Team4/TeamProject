ab6848d51b03f8fd7c84a337c83489b0181f9347
cjs1399 <cjs1399@naver.com>
2025-04-08 10:55:18 +0900

    [Setting] #6 - edit ImageLiterals

--- TeamProject/TeamProject/Global/Literals/ImageLiterals.swift
+++ TeamProject/TeamProject/Global/Literals/ImageLiterals.swift
@@ -8,5 +8,110 @@
 import UIKit
 
 enum ImageLiterals {
-    static var test: UIImage { .load(name: "test").withRenderingMode(.alwaysOriginal)}
+    enum Main {
+        static var airPodsMax: UIImage { .load(name: "AirPodsMax").withRenderingMode(.alwaysOriginal)}
+        static var airPodsPro2: UIImage { .load(name: "AirPodsPro2").withRenderingMode(.alwaysOriginal)}
+        static var applePencilPro: UIImage { .load(name: "ApplePencilPro").withRenderingMode(.alwaysOriginal)}
+        static var iMac: UIImage { .load(name: "iMac").withRenderingMode(.alwaysOriginal)}
+        static var iPad: UIImage { .load(name: "iPad").withRenderingMode(.alwaysOriginal)}
+        static var iPadAir: UIImage { .load(name: "iPadAir").withRenderingMode(.alwaysOriginal)}
+        static var iPadMini: UIImage { .load(name: "iPadMini").withRenderingMode(.alwaysOriginal)}
+        static var iPadPro: UIImage { .load(name: "iPadPro").withRenderingMode(.alwaysOriginal)}
+        static var iPhone15: UIImage { .load(name: "iPhone15").withRenderingMode(.alwaysOriginal)}
+        static var iPhone16: UIImage { .load(name: "iPhone16").withRenderingMode(.alwaysOriginal)}
+        static var iPhone16e: UIImage { .load(name: "iPhone16e").withRenderingMode(.alwaysOriginal)}
+        static var iPhone16Pro: UIImage { .load(name: "iPhone16Pro").withRenderingMode(.alwaysOriginal)}
+        static var macBookAir: UIImage { .load(name: "MacBookAir").withRenderingMode(.alwaysOriginal)}
+        static var macBookPro: UIImage { .load(name: "MacBookPro").withRenderingMode(.alwaysOriginal)}
+        static var macMini: UIImage { .load(name: "MacMini").withRenderingMode(.alwaysOriginal)}
+        static var macPro: UIImage { .load(name: "MacPro").withRenderingMode(.alwaysOriginal)}
+        static var macStudio: UIImage { .load(name: "MacStudio").withRenderingMode(.alwaysOriginal)}
+        static var magicKeyboard: UIImage { .load(name: "MagicKeyboard").withRenderingMode(.alwaysOriginal)}
+        static var magicMouse: UIImage { .load(name: "MagicMouse").withRenderingMode(.alwaysOriginal)}
+        static var magicTrackpad: UIImage { .load(name: "MagicTrackpad").withRenderingMode(.alwaysOriginal)}
+        static var magSafeCase: UIImage { .load(name: "MagSafeCase").withRenderingMode(.alwaysOriginal)}
+        static var proDisplayXDR: UIImage { .load(name: "ProDisplayXDR").withRenderingMode(.alwaysOriginal)}
+        static var studioDisplay: UIImage { .load(name: "StudioDisplay").withRenderingMode(.alwaysOriginal)}
+    }
+    
+    enum Detail {
+        static var airPodsMax_Blue: UIImage { .load(name: "AirPodsMax_Blue").withRenderingMode(.alwaysOriginal)}
+        static var airPodsMax_Midnight: UIImage { .load(name: "AirPodsMax_Midnight").withRenderingMode(.alwaysOriginal)}
+        static var airPodsMax_Orange: UIImage { .load(name: "AirPodsMax_Orange").withRenderingMode(.alwaysOriginal)}
+        static var airPodsMax_Purple: UIImage { .load(name: "AirPodsMax_Purple").withRenderingMode(.alwaysOriginal)}
+        static var airPodsMax_Starlight: UIImage { .load(name: "AirPodsMax_Starlight").withRenderingMode(.alwaysOriginal)}
+        static var airPodsPro2_White: UIImage { .load(name: "AirPodsPro2_White").withRenderingMode(.alwaysOriginal)}
+        static var applePencilPro_White: UIImage { .load(name: "ApplePencilPro_White").withRenderingMode(.alwaysOriginal)}
+        static var iMac_Blue: UIImage { .load(name: "iMac_Blue").withRenderingMode(.alwaysOriginal)}
+        static var iMac_Green: UIImage { .load(name: "iMac_Green").withRenderingMode(.alwaysOriginal)}
+        static var iMac_Orange: UIImage { .load(name: "iMac_Orange").withRenderingMode(.alwaysOriginal)}
+        static var iMac_Pink: UIImage { .load(name: "iMac_Pink").withRenderingMode(.alwaysOriginal)}
+        static var iMac_Purple: UIImage { .load(name: "iMac_Purple").withRenderingMode(.alwaysOriginal)}
+        static var iMac_Silver: UIImage { .load(name: "iMac_Silver").withRenderingMode(.alwaysOriginal)}
+        static var iMac_Yellow: UIImage { .load(name: "iMac_Yellow").withRenderingMode(.alwaysOriginal)}
+        static var iPad_Blue: UIImage { .load(name: "iPad_Blue").withRenderingMode(.alwaysOriginal)}
+        static var iPad_Pink: UIImage { .load(name: "iPad_Pink").withRenderingMode(.alwaysOriginal)}
+        static var iPad_Silver: UIImage { .load(name: "iPad_Silver").withRenderingMode(.alwaysOriginal)}
+        static var iPad_Yellow: UIImage { .load(name: "iPad_Yellow").withRenderingMode(.alwaysOriginal)}
+        static var iPadAir_Blue: UIImage { .load(name: "iPadAir_Blue").withRenderingMode(.alwaysOriginal)}
+        static var iPadAir_Purple: UIImage { .load(name: "iPadAir_Purple").withRenderingMode(.alwaysOriginal)}
+        static var iPadAir_SpaceGray: UIImage { .load(name: "iPadAir_SpaceGray").withRenderingMode(.alwaysOriginal)}
+        static var iPadAir_Starlight: UIImage { .load(name: "iPadAir_Starlight").withRenderingMode(.alwaysOriginal)}
+        static var iPadMini_Blue: UIImage { .load(name: "iPadMini_Blue").withRenderingMode(.alwaysOriginal)}
+        static var iPadMini_Purple: UIImage { .load(name: "iPadMini_Purple").withRenderingMode(.alwaysOriginal)}
+        static var iPadMini_SpaceGray: UIImage { .load(name: "iPadMini_SpaceGray").withRenderingMode(.alwaysOriginal)}
+        static var iPadMini_Starlight: UIImage { .load(name: "iPadMini_Starlight").withRenderingMode(.alwaysOriginal)}
+        static var iPadPro_Silver: UIImage { .load(name: "iPadPro_Silver").withRenderingMode(.alwaysOriginal)}
+        static var iPadPro_SpaceBlack: UIImage { .load(name: "iPadPro_SpaceBlack").withRenderingMode(.alwaysOriginal)}
+        static var iPhone15_Black: UIImage { .load(name: "iPhone15_Black").withRenderingMode(.alwaysOriginal)}
+        static var iPhone15_Blue: UIImage { .load(name: "iPhone15_Blue").withRenderingMode(.alwaysOriginal)}
+        static var iPhone15_Green: UIImage { .load(name: "iPhone15_Green").withRenderingMode(.alwaysOriginal)}
+        static var iPhone15_Pink: UIImage { .load(name: "iPhone15_Pink").withRenderingMode(.alwaysOriginal)}
+        static var iPhone15_Yellow: UIImage { .load(name: "iPhone15_Yellow").withRenderingMode(.alwaysOriginal)}
+        static var iPhone16_Black: UIImage { .load(name: "iPhone16_Black").withRenderingMode(.alwaysOriginal)}
+        static var iPhone16_Pink: UIImage { .load(name: "iPhone16_Pink").withRenderingMode(.alwaysOriginal)}
+        static var iPhone16_Teal: UIImage { .load(name: "iPhone16_Teal").withRenderingMode(.alwaysOriginal)}
+        static var iPhone16_Ultramarine: UIImage { .load(name: "iPhone16_Ultramarine").withRenderingMode(.alwaysOriginal)}
+        static var iPhone16_White: UIImage { .load(name: "iPhone16_White").withRenderingMode(.alwaysOriginal)}
+        static var iPhone16e_Black: UIImage { .load(name: "iPhone16e_Black").withRenderingMode(.alwaysOriginal)}
+        static var iPhone16e_White: UIImage { .load(name: "iPhone16e_White").withRenderingMode(.alwaysOriginal)}
+        static var iPhone16Pro_BlackTitanium: UIImage { .load(name: "iPhone16Pro_BlackTitanium").withRenderingMode(.alwaysOriginal)}
+        static var iPhone16Pro_DesertTitanium: UIImage { .load(name: "iPhone16Pro_DesertTitanium").withRenderingMode(.alwaysOriginal)}
+        static var iPhone16Pro_NaturalTitanium: UIImage { .load(name: "iPhone16Pro_NaturalTitanium").withRenderingMode(.alwaysOriginal)}
+        static var iPhone16Pro_WhiteTitanium: UIImage { .load(name: "iPhone16Pro_WhiteTitanium").withRenderingMode(.alwaysOriginal)}
+        static var macBookAir_Midnight: UIImage { .load(name: "MacBookAir_Midnight").withRenderingMode(.alwaysOriginal)}
+        static var macBookAir_Silver: UIImage { .load(name: "MacBookAir_Silver").withRenderingMode(.alwaysOriginal)}
+        static var macBookAir_SkyBlue: UIImage { .load(name: "MacBookAir_SkyBlue").withRenderingMode(.alwaysOriginal)}
+        static var macBookAir_Starlight: UIImage { .load(name: "MacBookAir_Starlight").withRenderingMode(.alwaysOriginal)}
+        static var macBookPro_Silver: UIImage { .load(name: "MacBookPro_Silver").withRenderingMode(.alwaysOriginal)}
+        static var macBookPro_SpaceBlack: UIImage { .load(name: "MacBookPro_SpaceBlack").withRenderingMode(.alwaysOriginal)}
+        static var macMini_Silver: UIImage { .load(name: "MacMini_Silver").withRenderingMode(.alwaysOriginal)}
+        static var macPro_Silver: UIImage { .load(name: "MacPro_Silver").withRenderingMode(.alwaysOriginal)}
+        static var macStudio_Silver: UIImage { .load(name: "MacStudio_Silver").withRenderingMode(.alwaysOriginal)}
+        static var magicKeyboard_Black: UIImage { .load(name: "MagicKeyboard_Black").withRenderingMode(.alwaysOriginal)}
+        static var magicKeyboard_White: UIImage { .load(name: "MagicKeyboard_White").withRenderingMode(.alwaysOriginal)}
+        static var magicMouse_Black: UIImage { .load(name: "MagicMouse_Black").withRenderingMode(.alwaysOriginal)}
+        static var magicMouse_White: UIImage { .load(name: "MagicMouse_White").withRenderingMode(.alwaysOriginal)}
+        static var magicTrackpad_Black: UIImage { .load(name: "MagicTrackpad_Black").withRenderingMode(.alwaysOriginal)}
+        static var magicTrackpad_White: UIImage { .load(name: "MagicTrackpad_White").withRenderingMode(.alwaysOriginal)}
+        static var magSafeCase_Aquamarine: UIImage { .load(name: "MagSafeCase_Aquamarine").withRenderingMode(.alwaysOriginal)}
+        static var magSafeCase_Peony: UIImage { .load(name: "MagSafeCase_Peony").withRenderingMode(.alwaysOriginal)}
+        static var magSafeCase_Periwinkle: UIImage { .load(name: "MagSafeCase_Periwinkle").withRenderingMode(.alwaysOriginal)}
+        static var magSafeCase_StarFruit: UIImage { .load(name: "MagSafeCase_StarFruit").withRenderingMode(.alwaysOriginal)}
+        static var magSafeCase_Tangerine: UIImage { .load(name: "MagSafeCase_Tangerine").withRenderingMode(.alwaysOriginal)}
+        static var studioDisplay_Silver: UIImage { .load(name: "StudioDisplay_Silver").withRenderingMode(.alwaysOriginal)}
+        static var studioDisplayXDR_Silver: UIImage { .load(name: "StudioDisplayXDR_Silver").withRenderingMode(.alwaysOriginal)}
+    }
+    
+    enum iCon {
+        static var add_circle_black_ic: UIImage { .load(name: "add_circle_black_ic").withRenderingMode(.alwaysOriginal)}
+        static var add_circle_white_ic: UIImage { .load(name: "add_circle_white_ic").withRenderingMode(.alwaysOriginal)}
+        static var remove_circle_black_ic: UIImage { .load(name: "remove_circle_black_ic").withRenderingMode(.alwaysOriginal)}
+        static var remove_circle_white_ic: UIImage { .load(name: "remove_circle_white_ic").withRenderingMode(.alwaysOriginal)}
+        static var close_button_darkMode_ic: UIImage { .load(name: "close_button_dark_Mode_ic").withRenderingMode(.alwaysOriginal)}
+        static var close_button_lightMode_ic: UIImage { .load(name: "close_button_light_Mode_ic").withRenderingMode(.alwaysOriginal)}
+        static var trash_black_ic: UIImage { .load(name: "trash_black_ic").withRenderingMode(.alwaysOriginal)}
+        static var trash_white_ic: UIImage { .load(name: "trash_white_ic").withRenderingMode(.alwaysOriginal)}
+    }
+
 }
