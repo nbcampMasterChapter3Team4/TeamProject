50bb96a63e9f3fe3eedd2632a70c493aaf2a997c
seoyounghan <happyunbd364@naver.com>
2025-04-10 21:29:03 +0900

    [Feat] #35 - custombutton 추가

---
+++ TeamProject/TeamProject/Presentation/DetailModal/Views/CustomRadioButton.swift
@@ -0,0 +1,68 @@
+import UIKit
+
+import SnapKit
+import Then
+
+class CustomRadioButton: BaseView {
+    
+    // MARK: - UI Components
+    
+    private let radioButton = UIButton().then {
+        $0.clipsToBounds = true
+        $0.layer.cornerRadius = (SizeLiterals.Screen.screenWidth * 32 / 402) / 2
+    }
+    private let radioBorderView = UIView().then {
+        $0.backgroundColor = .clear
+        $0.layer.borderWidth = 2
+        //$0.layer.borderColor = UIColor.black100.cgColor
+    }
+    
+    // MARK: - Properties
+
+    private var isSelectedColor = false
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        radioBorderView.layer.cornerRadius = radioBorderView.bounds.width / 2
+    }
+    
+    override func setStyles() {
+        self.backgroundColor = .clear
+    }
+    
+    // MARK: - Layout Helper
+    
+    override func setLayout() {
+        addSubviews(radioBorderView)
+        radioBorderView.addSubviews(radioButton)
+        
+        radioButton.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.width.height.equalTo(SizeLiterals.Screen.screenWidth * 32 / 402)
+        }
+        
+        radioBorderView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+            $0.width.height.equalTo(SizeLiterals.Screen.screenWidth * 40 / 402)
+        }
+    }
+    
+    // MARK: - Methods
+    
+    func configure(with colors: IEColor) {
+        radioButton.backgroundColor = colors.colorValue
+    }
+    
+    func setSelected(_ isSelected: Bool) {
+        print(isSelected)
+        radioBorderView.layer.borderColor = isSelected ? UIColor.blue200.cgColor : UIColor.black100.cgColor
+    }
+
+    func getRadioButton() -> UIButton {
+        return radioButton
+    }
+    
+    func getRadioBorderView() -> UIView {
+        return radioBorderView
+    }
+}
--- TeamProject/TeamProject/Presentation/DetailModal/Views/DetailColorsStackView.swift
+++ TeamProject/TeamProject/Presentation/DetailModal/Views/DetailColorsStackView.swift
@@ -1,5 +1,4 @@
 import UIKit
-
 import SnapKit
 import Then
 
@@ -9,17 +8,29 @@ class DetailColorsStackView: UIStackView {
 
     var colorSelectedHandler: ((IEColor) -> Void)?
     private var colors: [IEColor] = []
+    private var radioButtons: [CustomRadioButton] = []
 
-    /// View 의 Style 을 set 합니다.
+    /// View의 Style 설정
     func setStyles() {
         self.axis = .horizontal
-        self.spacing = 8
+        self.spacing = 4
         self.alignment = .center
         self.distribution = .equalSpacing
     }
 
-    // MARK: - Methods
+    // MARK: - Initializer
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setStyles()
+    }
     
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Methods
+
     func updateContent(with colors: [IEColor]) {
         self.colors = colors
 
@@ -27,29 +38,36 @@ class DetailColorsStackView: UIStackView {
             removeArrangedSubview(subview)
             subview.removeFromSuperview()
         }
+        radioButtons.removeAll()
 
-        for color in colors {
-            let colorButton = UIButton(type: .system)
-            colorButton.layer.cornerRadius = 16
-            colorButton.clipsToBounds = true
-            colorButton.backgroundColor = color.colorValue
-            colorButton.tag = color.hashValue
-            colorButton.snp.makeConstraints {
-                $0.width.height.equalTo(32)
-            }
+        for (index, color) in colors.enumerated() {
+            let radioButton = CustomRadioButton()
+            radioButton.setStyles()
+            radioButton.setLayout()
+            radioButton.configure(with: color)
 
-            colorButton.addTarget(self, action: #selector(colorButtonTapped(_:)), for: .touchUpInside)
+            radioButton.getRadioButton().tag = index
 
-            addArrangedSubview(colorButton)
+            radioButton.getRadioButton().addTarget(self, action: #selector(didTapRadio(_:)), for: .touchUpInside)
+
+            addArrangedSubview(radioButton)
+            radioButtons.append(radioButton)
         }
     }
 
     // MARK: - @objc Methods
 
-    @objc private func colorButtonTapped(_ sender: UIButton) {
-        guard let selectedColor = colors.first(where: { $0.hashValue == sender.tag }) else {
-            return
+    @objc private func didTapRadio(_ sender: UIButton) {
+        let newIndex = sender.tag
+
+        // 모든 버튼에 대해 선택 상태 업데이트 (선택된 버튼만 true)
+        for (index, button) in radioButtons.enumerated() {
+            button.setSelected(index == newIndex)
         }
+
+        // 선택된 색상을 핸들러를 통해 외부로 전달
+        let selectedColor = colors[newIndex]
         colorSelectedHandler?(selectedColor)
+        print("선택된 인덱스: \(newIndex), 색상: \(selectedColor)")
     }
 }
